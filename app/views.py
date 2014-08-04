49fa93faf0c0ad609ba72e959e6d9c10e90e6d84
niuniu <playniuniu@gmail.com>
2014-08-04 17:46:31 +0800

    reconstruct idc user
    upgrade AdminLTE template

--- app/views.py
+++ app/views.py
@@ -8,84 +8,85 @@ from flask import render_template
 def index():
     return render_template('index.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'客户概览',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='客户概览',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
 
 
 @app.route('/maintain')
 def maintain():
     return render_template('pages/maintain.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'设备维护',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='设备维护',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
 
 
 @app.route('/safe')
 def safe():
     return render_template('pages/safe.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'安全服务',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='安全服务',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
 
 @app.route('/seo')
 def seo():
     return render_template('pages/iframe.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'SEO服务',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户',
+        content_title='SEO服务',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户',
         web_url="http://cn.majesticseo.com/")
 
 @app.route('/data')
 def data():
     return render_template('pages/data.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'数据服务',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='数据服务',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
 
 @app.route('/report')
 def report():
     return render_template('pages/report.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'月度报表',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='月度报表',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
 
 @app.route('/device')
 def device():
     return render_template('pages/device.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'机房资产',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='机房资产',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
 
 
 @app.route('/flow')
 def flow():
     return render_template('pages/flow.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'流量统计',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='流量统计',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
 
 
 @app.route('/billing')
 def billing():
     return render_template('pages/billing.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'费用查询',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='费用查询',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
 
 
 @app.route('/buy')
 def buy():
     return render_template('pages/buy.html',
         web_title=app.config['WEB_TITLE'],
-        content_title=u'企业套餐',
-        user_name=u'于旸',
-        user_role=u'金石IDC客户')
+        content_title='企业套餐',
+        user_name='北京蓝汛',
+        user_role='金石IDC客户')
+
--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 # coding: utf-8
 
-import os
-
-IDC_ROOT = os.path.dirname(os.path.abspath(__file__))
-APP_ROOT = os.path.join(IDC_ROOT, 'app')
-
 DEBUG = True
-WEB_TITLE = 'IDC Online'
+WEB_TITLE = 'uKer'
